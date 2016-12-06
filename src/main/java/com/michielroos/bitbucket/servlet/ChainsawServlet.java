a0e4d99fd95f7268373f29d495d7e6a7ccadf3c5
Michiel Roos <michiel@maxserv.com>
2016-12-06 20:44:05 +0100

    Working template!

--- src/main/java/com/michielroos/bitbucket/servlet/ChainsawServlet.java
+++ src/main/java/com/michielroos/bitbucket/servlet/ChainsawServlet.java
@@ -1,37 +1,55 @@
 package com.michielroos.bitbucket.servlet;
 
+/**
+ * Copyright notice
+ *
+ * ⓒ 2016 Michiel Roos <michiel@maxserv.nl>
+ * All rights reserved
+ *
+ * This script is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ */
+
 import com.atlassian.bitbucket.permission.PermissionService;
+import com.atlassian.bitbucket.repository.Repository;
 import com.atlassian.bitbucket.repository.RepositoryService;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
-import com.atlassian.templaterenderer.*;
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-@Component
+@Scanned
 public class ChainsawServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(ChainsawServlet.class);
-    private final RepositoryService repositoryService;
-    private final PermissionService permissionService;
-//    private final TemplateRenderer templateRenderer;
 
-    @Autowired
-//    public ChainsawServlet(@ComponentImport RepositoryService repositoryService, @ComponentImport PermissionService permissionService, @ComponentImport TemplateRenderer templateRenderer) {
-    public ChainsawServlet(@ComponentImport RepositoryService repositoryService, @ComponentImport PermissionService permissionService) {
+    @ComponentImport
+    private final RepositoryService repositoryService;
+    @ComponentImport
+    private final PermissionService permissionService;
+
+    @Inject
+    public ChainsawServlet(RepositoryService repositoryService, PermissionService permissionService) {
         this.repositoryService = checkNotNull(repositoryService);
         this.permissionService = checkNotNull(permissionService);
-//        this.templateRenderer = checkNotNull(templateRenderer);
     }
 
     @Override
@@ -55,16 +73,28 @@ public class ChainsawServlet extends HttpServlet {
         log.warn(components[1]);
         log.warn(components[2]);
 
-//        Repository repository = repositoryService.getBySlug(components[1], components[2]);
-//
-//        if (repository == null) {
-//            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-//            return;
-//        }
+        Repository repository = repositoryService.getBySlug(components[1], components[2]);
 
-        log.warn("aaaaaah");
+        if (repository == null) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
 
-//        resp.setContentType("text/html;charset=utf-8");
-//        renderer.render("templates/chainsaw.vm", ImmutableMap.<String, Object>of("repository", repository), resp.getWriter());
+        Map<String, Object> parameters = new HashedMap();
+        parameters.put("repository", repository);
+        log.warn("form parametrs : " + parameters);
+        VelocityContext context = new VelocityContext(parameters);
+        VelocityEngine engine = new VelocityEngine();
+        engine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        engine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        engine.setProperty("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.NullLogSystem");
+        try {
+            engine.init();
+            Template template = engine.getTemplate("templates/chainsaw.vm", "UTF-8");
+            resp.setContentType("text/html;charset=utf-8");
+            template.merge(context, resp.getWriter());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
